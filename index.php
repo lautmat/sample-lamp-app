5820326137652df687f30549cbe2726587bb75e6
Laurent Mathieu <67669940+lautmat@users.noreply.github.com>
2023-02-09 08:55:10 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+php
+<html>
+  <head>
+    <title>Sample LAMP Application</title>
+  </head>
+  <body>
+    <h1>Hello, World!</h1>
+    <p>This is a sample LAMP application.</p>
+  </body>
+</html>
