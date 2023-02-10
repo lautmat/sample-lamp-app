8794532c5edd54e8cf7966a68feb6da0e17a1a64
Laurent Mathieu <67669940+lautmat@users.noreply.github.com>
2023-02-10 13:40:25 +0700

    change version 1.3

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 php
 <html>
   <head>
-    <title>Sample LAMP Application - version 1.2</title>
+    <title>Sample LAMP Application - version 1.3</title>
   </head>
   <body>
     <h1>Hello, World!</h1>
-    <p>Hi Savoeurn!!!</p>
+    <p>Hi Teachers!!! How are you doing today??</p>
+    <h2>version 1.3</h2>
   </body>
 </html>
