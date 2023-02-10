a0db2d52c087248b81f0b04e2c2b9d6c610d134d
Laurent Mathieu <67669940+lautmat@users.noreply.github.com>
2023-02-10 13:42:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 php
 <html>
   <head>
-    <title>Sample LAMP Application - version 1.3</title>
+    <title>Sample LAMP Application - version 1.4</title>
   </head>
   <body>
     <h1>Hello, World!</h1>
-    <p>Hi Teachers!!! How are you doing today??</p>
-    <h2>version 1.3</h2>
+    <p>Have a nice day!!</p>
+    <h2>version 1.4</h2>
   </body>
 </html>
